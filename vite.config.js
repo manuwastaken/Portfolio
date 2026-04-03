27d33575825490bb6d4df3214ca442d9f4674202
Manu <absy00002@stud.uni-saarland.de>
2026-04-03 22:16:53 +0200

    changed base path as renamed repo

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/Portfolio/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/" : "/",
   plugins: [react(),tailwindcss()],
 })
