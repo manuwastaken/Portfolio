98f5b723e93f885819ff0c12b05871cf1c29bf63
Manu <absy00002@stud.uni-saarland.de>
2026-04-03 22:01:45 +0200

    fixing the path for new dommain

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/Portfolio/" : "/",
   plugins: [react(),tailwindcss()],
 })
